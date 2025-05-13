2bdba0ef6d207374d1f961a9442611fcb3fab881
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-05-13 10:20:34 +0300

    SpringBoot project setup

---
+++ src/test/java/com/example/priceComparator/PriceComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priceComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
