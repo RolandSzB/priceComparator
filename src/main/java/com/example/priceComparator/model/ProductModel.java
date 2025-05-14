dfd301af67b45ee072f0f515d7a9f584c9266bb7
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-05-14 17:26:04 +0300

    added member variables to ProductModel constructor

--- src/main/java/com/example/priceComparator/model/ProductModel.java
+++ src/main/java/com/example/priceComparator/model/ProductModel.java
@@ -3,5 +3,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class ProductModel {
+    private String productId;
+    private String productName;
+    private String productCategory;
+    private String brand;
+    private Number packageQuantity;
+    private String packageUnit;
+    private Number price;
+    private String currency;
 }
