2b7c50c8405a011f577d4b30ee7ae1b84cf16758
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-05-14 19:03:48 +0300

    testing, if csv reader reads the data from the files correctly

--- src/main/java/com/example/priceComparator/PriceComparatorApplication.java
+++ src/main/java/com/example/priceComparator/PriceComparatorApplication.java
@@ -1,13 +1,19 @@
 package com.example.priceComparator;
 
-import org.springframework.boot.SpringApplication;
+import com.example.priceComparator.model.ProductModel;
+import com.example.priceComparator.util.CsvProductOfferReader;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class PriceComparatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PriceComparatorApplication.class, args);
-	}
+		List<ProductModel> offers = CsvProductOfferReader.readAllOffersFromFolder();
 
+		for (ProductModel offer : offers) {
+			System.out.println(offer);
+		}
+	}
 }
