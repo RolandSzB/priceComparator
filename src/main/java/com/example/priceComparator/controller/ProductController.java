fafcf37e0c9cc768808b2f33e571b32bf407df74
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-05-14 19:45:02 +0300

    offers displayed grouped per store

--- src/main/java/com/example/priceComparator/controller/ProductController.java
+++ src/main/java/com/example/priceComparator/controller/ProductController.java
@@ -2,17 +2,30 @@ package com.example.priceComparator.controller;
 
 import com.example.priceComparator.model.ProductModel;
 import com.example.priceComparator.util.CsvProductOfferReader;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/api/products")
+@Controller
 public class ProductController {
-    @GetMapping
-    public List<ProductModel> getAllProducts() {
-        return CsvProductOfferReader.readAllOffersFromFolder();
+
+    @GetMapping("/offers")
+    public String getAllOffers(Model model) {
+        List<ProductModel> offers = CsvProductOfferReader.readAllOffersFromFolder();
+        model.addAttribute("offers", offers);
+        return "offers";
+    }
+    @GetMapping("/offers/grouped")
+    public String getGroupedOffers(Model model) {
+        List<ProductModel> offers = CsvProductOfferReader.readAllOffersFromFolder();
+        Map<String, List<ProductModel>> groupedOffers = offers.stream()
+                .collect(Collectors.groupingBy(ProductModel::getStoreName));
+
+        model.addAttribute("groupedOffers", groupedOffers);
+        return "offers-grouped";
     }
 }
