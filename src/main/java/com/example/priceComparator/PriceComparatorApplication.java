48f13a023a355caf1e7566a3c5efde41d597900d
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-05-14 19:16:23 +0300

    csv  details displayed on localhost

--- src/main/java/com/example/priceComparator/PriceComparatorApplication.java
+++ src/main/java/com/example/priceComparator/PriceComparatorApplication.java
@@ -1,26 +1,12 @@
 package com.example.priceComparator;
 
-import com.example.priceComparator.model.ProductModel;
-import com.example.priceComparator.util.CsvProductOfferReader;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class PriceComparatorApplication {
 
 	public static void main(String[] args) {
-		List<ProductModel> offers = CsvProductOfferReader.readAllOffersFromFolder();
-
-		for (ProductModel product : offers) {
-			System.out.println("Magazin: " + product.getStoreName());
-			System.out.println("ID: " + product.getProductId());
-			System.out.println("Nume: " + product.getProductName());
-			System.out.println("Categorie: " + product.getProductCategory());
-			System.out.println("Brand: " + product.getBrand());
-			System.out.println("Cantitate: " + product.getPackageQuantity() + " " + product.getPackageUnit());
-			System.out.println("Preț: " + product.getPrice() + " " + product.getCurrency());
-			System.out.println("-----------");
-		}
+		SpringApplication.run(PriceComparatorApplication.class, args);
 	}
 }
